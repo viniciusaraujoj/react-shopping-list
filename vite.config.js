a306b0b15ab62c7e59654b82ecacd324bddf85ce
vinicius araujo <viniciusaraujoj>
2024-06-27 23:56:12 -0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-shopping-list/',
   server: {
     port: 3000,
   },
